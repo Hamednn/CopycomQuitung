602bd7e1b7f27ad49ffb0ec794dc0eceda8bbf6c
hamed <hamed@DESKTOP-624N37J>
2021-08-29 12:15:23 +0200

    Projektdateien hinzufügen.

---
+++ COCOQuitung/Artikel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COCOQuitung
+{
+    class Artikel
+    {
+        private string name;
+        private string discription;
+        private int amounts;
+        private double price;
+
+        public string get_name() {
+            return this.name;
+        }
+        public void set_name(string name) {
+            if (name.Length >= 3)
+            {
+                if (!string.IsNullOrEmpty(name))
+                {
+                    this.name = name;
+                }
+            }
+        }
+        public string get_dicription() { 
+            return this.discription;
+        }
+        public void set_discription(string input) {
+            this.discription = input;
+        }
+        public int get_amounts()
+        {
+            return this.amounts;
+        }
+        public void set_amounts(int input)
+        {
+            if (input >= 0) {
+                this.amounts = input;
+            }
+            this.amounts = 0;
+        }
+        
+        public double get_price()
+        {
+            return this.price;
+        }
+        public void set_price(double input)
+        {
+            if (input>= 0)
+            {
+                this.price = input;
+            }
+            else
+            {
+                this.price = 0;
+            }
+        }
+    }
+}
---
+++ COCOQuitung/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace COCOQuitung
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtPreis.Text = "0,00€";
+            txtMenge.Text = "0";
+           // this. += new EventHandler(lstSelectItem);
+
+        }
+        static int index = 0;
+        private void fillListBox() {
+            lstArtikel.Items.Clear();
+            lstdesc.Items.Clear();
+            foreach (Artikel a in artikelListe)
+            {
+                lstArtikel.Items.Add(a.get_name());
+                lstdesc.Items.Add(a.get_dicription());
+            }
+        }
+        List<Artikel> artikelListe = new List<Artikel>();
+        private void btnAddAction(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtartikel.Text) && !string.IsNullOrWhiteSpace(txtbeschreibung.Text))
+            {
+
+
+                Artikel neuArtikel = new Artikel();
+                neuArtikel.set_name(txtartikel.Text);
+                neuArtikel.set_discription(txtbeschreibung.Text);
+                neuArtikel.set_amounts(Int32.Parse(txtMenge.Text));
+                neuArtikel.set_price(double.Parse(txtPreis.Text));
+                artikelListe.Add(neuArtikel);
+                fillListBox();
+
+            }
+        }
+
+        private void lstSelectItem(object sender, System.EventArgs e)
+        {
+            
+            ListBox obj = (ListBox)sender;
+            if (obj.SelectedIndex != -1)
+            {
+                index = obj.SelectedIndex;
+                lstArtikel.SelectionMode = SelectionMode.Single;
+                lstdesc.SelectionMode = SelectionMode.Single;
+                if (obj == lstArtikel)
+                {
+                    lstdesc.SelectedIndex = index;
+                }
+                else
+                {
+                    lstArtikel.SelectedIndex = index;
+                }
+
+                
+            }
+        }
+
+        private void btnDeleteAction(object sender, RoutedEventArgs e)
+        {
+            if (lstArtikel.Items.Count > 0)
+            {
+                artikelListe.RemoveAt(index);
+                fillListBox();
+            }
+            else {
+                MessageBox.Show("Keine Elemente vorhanden");
+            }
+        }
+    }
+}
